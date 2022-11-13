ac06ad52b0da9e334ff8d26338bbb123fb6b3194
Nesim <45429767+nd55@users.noreply.github.com>
2022-11-13 15:57:56 +0100

    mobile nav

--- src/components/Header/MobileHeader.js
+++ src/components/Header/MobileHeader.js
@@ -1,4 +1,29 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("#hamburger")
-  const mobileNav = document.getElementById("#mobile-nav")
+  const hamburger = document.getElementById("hamburger")
+  const mobileNav = document.getElementById("mobile-nav")
+
+  hamburger.addEventListener("click", () => {
+    const isOpen = hamburger.getAttribute("aria-expanded") === "true"
+    hamburger.setAttribute("aria-expanded", isOpen ? "false" : "true")
+
+    if (!isOpen) {
+      mobileNav.querySelectorAll("[aria-haspopup='menu']").forEach((item) => {
+        item.setAttribute("aria-expanded", "false")
+      })
+    }
+  })
+
+  mobileNav.querySelectorAll("[aria-haspopup='menu']").forEach((item) => {
+    item.addEventListener("click", () => {
+      const isToggled = item.getAttribute("aria-expanded") === "true"
+      item.setAttribute("aria-expanded", isToggled ? "false" : "true")
+
+      // close button
+      item.nextElementSibling
+        .querySelector("li:first-child > button")
+        .addEventListener("click", () => {
+          item.setAttribute("aria-expanded", "false")
+        })
+    })
+  })
 })
